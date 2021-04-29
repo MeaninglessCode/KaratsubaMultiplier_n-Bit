13be190a469f36908c629a12ab64e505215b4ce7
MeaninglessCode <derek0taylor@gmail.com>
2021-04-28 23:24:30 -0500

    Fixing odd bit width bug

--- KaratsubaMultiplier_n-bit.sv
+++ KaratsubaMultiplier_n-bit.sv
@@ -37,35 +37,35 @@ module KaratsubaMultiplier_nbit #(parameter WIDTH=64) (
                 return (z2 << 4) + ((z1 - (z2 + z0)) << 2) + z0;
             end
             if ((KARAT_WIDTH % 2) == 0) begin
-                reg[KARAT_WIDTH - 1:0] z0 = Karatsuba#(KARAT_WIDTH / 2)::multiply(
-                    X[(KARAT_WIDTH / 2) - 1:0],
-                    Y[(KARAT_WIDTH / 2) - 1:0]
+                reg[KARAT_WIDTH - 1:0] z0 = Karatsuba#(int'(KARAT_WIDTH / 2))::multiply(
+                    X[(int'(KARAT_WIDTH / 2)) - 1:0],
+                    Y[(int'(KARAT_WIDTH / 2)) - 1:0]
                 );
                 
-                reg[KARAT_WIDTH + 1:0] z1 = Karatsuba#((KARAT_WIDTH / 2) + 1)::multiply(
-                    X[KARAT_WIDTH - 1:KARAT_WIDTH / 2] + X[(KARAT_WIDTH / 2) - 1:0],
-                    Y[KARAT_WIDTH - 1: KARAT_WIDTH / 2] + Y[(KARAT_WIDTH / 2) - 1:0]
+                reg[KARAT_WIDTH + 1:0] z1 = Karatsuba#((int'(KARAT_WIDTH / 2)) + 1)::multiply(
+                    X[KARAT_WIDTH - 1:int'(KARAT_WIDTH / 2)] + X[(int'(KARAT_WIDTH / 2)) - 1:0],
+                    Y[KARAT_WIDTH - 1: int'(KARAT_WIDTH / 2)] + Y[(int'(KARAT_WIDTH / 2)) - 1:0]
                 );
                 
-                reg[KARAT_WIDTH - 1:0] z2 = Karatsuba#(KARAT_WIDTH / 2)::multiply(
-                    X[KARAT_WIDTH - 1:KARAT_WIDTH / 2],
-                    Y[KARAT_WIDTH - 1:KARAT_WIDTH / 2]
+                reg[KARAT_WIDTH - 1:0] z2 = Karatsuba#(int'(KARAT_WIDTH / 2))::multiply(
+                    X[KARAT_WIDTH - 1:int'(KARAT_WIDTH / 2)],
+                    Y[KARAT_WIDTH - 1:int'(KARAT_WIDTH / 2)]
                 );
                 
-                return (z2 << KARAT_WIDTH) + ((z1 - (z2 + z0)) << (KARAT_WIDTH / 2)) + z0;
+                return (z2 << KARAT_WIDTH) + ((z1 - (z2 + z0)) << (int'(KARAT_WIDTH / 2))) + z0;
             end
             else if ((KARAT_WIDTH % 2) != 0) begin
-                reg[KARAT_WIDTH - 2:0] z0 = Karatsuba#((KARAT_WIDTH / 2) + 1)::multiply(
-                    X[(KARAT_WIDTH / 2) - 1:0], Y[(KARAT_WIDTH / 2) - 1:0]);
+                reg[KARAT_WIDTH - 2:0] z0 = Karatsuba#((int'(KARAT_WIDTH / 2)) + 1)::multiply(
+                    X[(int'(KARAT_WIDTH / 2)) - 1:0], Y[(int'(KARAT_WIDTH / 2)) - 1:0]);
                 
-                reg[KARAT_WIDTH:0] z1 = Karatsuba#((KARAT_WIDTH / 2) + 1)::multiply(
-                    X[KARAT_WIDTH - 1:KARAT_WIDTH / 2] + X[(KARAT_WIDTH / 2) - 1:0],
-                    Y[KARAT_WIDTH - 1:KARAT_WIDTH / 2] + Y[(KARAT_WIDTH / 2) - 1:0]
+                reg[KARAT_WIDTH:0] z1 = Karatsuba#((int'(KARAT_WIDTH / 2)) + 1)::multiply(
+                    X[KARAT_WIDTH - 1:int'(KARAT_WIDTH / 2)] + X[(int'(KARAT_WIDTH / 2)) - 1:0],
+                    Y[KARAT_WIDTH - 1:int'(KARAT_WIDTH / 2)] + Y[(int'(KARAT_WIDTH / 2)) - 1:0]
                 );
                 
-                reg[KARAT_WIDTH - 2:0] z2 = Karatsuba#((KARAT_WIDTH / 2) + 1)::multiply(
-                    X[KARAT_WIDTH - 1:KARAT_WIDTH / 2],
-                    Y[KARAT_WIDTH - 1:KARAT_WIDTH / 2]
+                reg[KARAT_WIDTH - 2:0] z2 = Karatsuba#((int'(KARAT_WIDTH / 2)) + 1)::multiply(
+                    X[KARAT_WIDTH - 1:int'(KARAT_WIDTH / 2)],
+                    Y[KARAT_WIDTH - 1:int'(KARAT_WIDTH / 2)]
                 );
                 
                 return (z2 << (KARAT_WIDTH - 1)) + ((z1 - (z2 + z0)) << ((KARAT_WIDTH - 1) / 2)) + z0;
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # KaratsubaMultiplier-nBit
-A generic width Karatsuba multiplier. Currently has a small bug with odd widths due to division behavior. I may or may not fix it, but it should be fairly trivial to do so.
+A generic width Karatsuba multiplier.
